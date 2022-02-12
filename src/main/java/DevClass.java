8728a3db2ffa49f1626a420bea7b1ee6404e71fc
Quỳnh <thanhquynh610@gmail.com>
2022-02-12 18:20:51 +0700

    Added more changes (#2)

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("DevClass-Stream2Learn");
+        System.out.println("DevClass-Stream2Learn-123");
         //Checkin from Stream2 Learn
     }
 }
